56efdb2c324fe768a3f88e037e779702e17086ec
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2022-08-09 19:32:27 +0100

    switch back to redis.com provider

--- db/redis.js
+++ db/redis.js
@@ -2,19 +2,9 @@ import Redis from "ioredis";
 import config from "../config/index.js";
 
 // const redis = new Redis();
-// const redis = new Redis({
-//     host: config.REDIS_HOST,
-//     port: config.REDIS_PORT,
-//     password: config.REDIS_PASSWORD,
-//     tls: {
-//         host: config.REDIS_HOST,
-//     },
-// });
-
-const redis = new Redis(config.REDIS_URL, {
-    connectTimeout: 10000,
-    tls: {
-        rejectUnauthorized: false
-    }
+const redis = new Redis({
+    host: config.REDIS_HOST,
+    port: config.REDIS_PORT,
+    password: config.REDIS_PASSWORD
 });
 export default redis;
