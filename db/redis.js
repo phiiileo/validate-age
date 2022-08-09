b06d7639613e2ed58b2a9c300452fa1b323228a3
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2022-08-09 20:10:46 +0100

    formatted using prettier

--- db/redis.js
+++ db/redis.js
@@ -1,10 +1,10 @@
-const Redis = require("ioredis");
-const config = require("../config/index.js");
+const Redis = require('ioredis')
+const config = require('../config/index.js')
 
 // const redis = new Redis();
 const redis = new Redis({
-    host: config.REDIS_HOST,
-    port: config.REDIS_PORT,
-    password: config.REDIS_PASSWORD
-});
-export default redis;
+  host: config.REDIS_HOST,
+  port: config.REDIS_PORT,
+  password: config.REDIS_PASSWORD,
+})
+export default redis
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const rate_limit = require("../middleware/rate_limit.js");
-const age_route = require('./age_route.js');
-const base_route = require('./base_route.js');
-const router = express.Router();
+const express = require('express')
+const rate_limit = require('../middleware/rate_limit.js')
+const age_route = require('./age_route.js')
+const base_route = require('./base_route.js')
+const router = express.Router()
 
 // Age route wrapper
-router.use("/howold", rate_limit.rateLimit, age_route);
+router.use('/howold', rate_limit.rateLimit, age_route)
 // Home route wrapper
-router.use("/", base_route);
+router.use('/', base_route)
 
-export default router;
+export default router
