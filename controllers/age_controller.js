d449ba89266061f7c82fe9c69327835a6e9c6336
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2022-08-10 22:09:04 +0100

    changed the status code to 422 for undefined and null values

--- controllers/age_controller.js
+++ controllers/age_controller.js
@@ -5,6 +5,8 @@ const ValidationError = require(`../helpers/ValidationError.js`);
 const calculateAge = async (req, res) => {
   try {
     const dob = req.query.dob;
+    if (dob == 'undefined') throw new ValidationError(`dob cannot be undefined`, 422);
+    if (dob == 'null') throw new ValidationError(`dob cannot be null`, 422);
     if (!dob) throw new ValidationError(`Please pass dob has query parameter`);
     // calculate age from query
     const age = age_service.calaculateAge(dob);
@@ -12,7 +14,7 @@ const calculateAge = async (req, res) => {
     response.SuccessResponse(res, age);
   } catch (error) {
     if (error instanceof ValidationError)
-      return response.BadRequestResponse(res, error.message);
+      return response.BadRequestResponse(res, error.message, error.statuscode);
     return response.ServerError(res, error);
   }
 };
