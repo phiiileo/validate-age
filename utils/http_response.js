f1f0439087d5d67f3453e8c9ec0c7b0eeabffca4
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2022-08-12 01:29:09 +0100

    fix

--- utils/http_response.js
+++ utils/http_response.js
@@ -13,20 +13,23 @@ const BadRequestResponse = (res, message, statuscode = 400) => {
 // format http too many request response
 const TooManyRequestResponse = (res, timeleft) => {
   res.statusCode = 429;
-  res.end(JSON.stringify(
-    {
-      error: `Too many request please wait for ${timeleft} milliseconds to try again`
-    }
-    ));
+  res.end(
+        JSON.stringify(
+        {
+            error: `Too many request please wait for ${timeleft} milliseconds to try again`,
+        }
+     ));
 };
 
 // format http server error response
 const ServerError = (res, error, message) => {
   console.log(error);
   res.statusCode = 500;
-  res.end(JSON.stringify({
-      error: 
-        message || `Server error. Something unexpected happened please contact support!`
+  res.end(
+        JSON.stringify(
+          {
+              error: 
+                message || `Server error. Something unexpected happened please contact support!`
   }));
 };
 
