1fafff48b5b05eae7f7e3a03e5e2c8187bba7066
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2022-08-12 01:34:05 +0100

    fix

--- utils/http_response.js
+++ utils/http_response.js
@@ -2,40 +2,40 @@
 const SuccessResponse = (res, message) => {
   res.statusCode = 200;
   res.end(JSON.stringify({ age: message }));
-};
+}
 
 // format http bad request response
 const BadRequestResponse = (res, message, statuscode = 400) => {
   res.statusCode = statuscode || 400;
   res.end(JSON.stringify({ error: message }));
-};
+}
 
 // format http too many request response
 const TooManyRequestResponse = (res, timeleft) => {
   res.statusCode = 429;
   res.end(
-        JSON.stringify(
-        {
-            error: `Too many request please wait for ${timeleft} milliseconds to try again`,
-        }
-     ));
-};
+    JSON.stringify({
+      error: `Too many request please wait for ${timeleft} milliseconds to try again`,
+    }),
+  );
+}
 
 // format http server error response
 const ServerError = (res, error, message) => {
   console.log(error);
   res.statusCode = 500;
   res.end(
-        JSON.stringify(
-          {
-              error: 
-                message || `Server error. Something unexpected happened please contact support!`
-  }));
-};
+    JSON.stringify({
+      error:
+        message ||
+        `Server error. Something unexpected happened please contact support!`,
+    }),
+  );
+}
 
 module.exports = {
   SuccessResponse,
   BadRequestResponse,
   ServerError,
   TooManyRequestResponse,
-};
+}
