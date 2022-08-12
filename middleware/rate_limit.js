62c4463cde2011a1b4c48ba096f4dce70481248e
Emmanuel Owojori <emmanuelboluwajib1@gmail.com>
2022-08-12 01:48:09 +0100

    fix

--- middleware/rate_limit.js
+++ middleware/rate_limit.js
@@ -21,7 +21,11 @@ const rateLimit = async (req, res, next) => {
   } //else ttl = await redis.ttl(ipaddress);
   //check if number of request is greater than allowed number, if yes return error
   return iprequest > numberofrequest
-    ? response.TooManyRequestResponse(res, ttl || 1, numberofrequest - ipaddress)
+    ? response.TooManyRequestResponse(
+      res,
+      ttl || 1,
+      numberofrequest - ipaddress
+      )
     : next();
 };
 module.exports = {
